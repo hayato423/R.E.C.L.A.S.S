43061083acdde6c6f9fee9ff07c086a74e797bcf
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-06-16 21:15:45 +0900

    'スレッド処理を追加'

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import home
+import threading
+import datetime
+import time
+import sys
 
 
 home_window = home.Home()
-home_window.open()
+
+def func():
+  while True:
+    dt_now = datetime.datetime.now()
+    print(dt_now)
+    time.sleep(1)
+
+
+if __name__== '__main__':
+  thread_1 = threading.Thread(target=home_window.open)
+  thread_2 = threading.Thread(target=func)
+
+  thread_1.start()
+  thread_2.start()
