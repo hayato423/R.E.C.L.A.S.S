450c83a7b49f685d8a85fec4285af6b42e0704e5
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-06-20 03:44:53 +0900

    scomb_login.pyを追加

--- get_timetable.py
+++ get_timetable.py
@@ -1,36 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
-import chromedriver_binary
-
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver,20)
+from scomb_login import login_confirm
 
 ID = 'al18080'
 PASSWORD = '3490WaRaSu!9745'
 
-error_flg = False
+can_login , msg = login_confirm(ID,PASSWORD)
+print(msg)
 
-scomb_url = 'https://scomb.shibaura-it.ac.jp/portal/index'
 
-driver.get(scomb_url)
-wait.until(EC.presence_of_all_elements_located)
-try:
-  login_button = driver.find_element_by_id("loginField")
-  login_button.click()
-  wait.until(EC.presence_of_all_elements_located)
-  cur_url = driver.current_url
-  trim_url = str(cur_url).replace('https://','')
-  basic_auth_url = 'https://' + ID + '%40sic:' + PASSWORD + '@' + trim_url
-  driver.get(basic_auth_url)
-  wait.until(EC.presence_of_all_elements_located)
-  next_button = driver.find_element_by_id('continueButton')
-  next_button.click()
-
-except Exception as e:
-  error_flg = True
-  print(e)
