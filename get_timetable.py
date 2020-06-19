c6c7f28ad02c60c20b862dbfc1e2c963e1127b21
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-06-20 03:06:58 +0900

    scombにログイン成功

--- get_timetable.py
+++ get_timetable.py
@@ -3,13 +3,14 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 import chromedriver_binary
 
 driver = webdriver.Chrome()
-wait = WebDriverWait(driver,10)
+wait = WebDriverWait(driver,20)
 
-ID = 'al18080@sic'
+ID = 'al18080'
 PASSWORD = '3490WaRaSu!9745'
 
 error_flg = False
@@ -21,12 +22,15 @@ wait.until(EC.presence_of_all_elements_located)
 try:
   login_button = driver.find_element_by_id("loginField")
   login_button.click()
-  wait.until(EC.alert_is_present())
-  Alert(driver).send_keys(ID + Keys.TAB + PASSWORD)
-  Alert(driver).accept()
   wait.until(EC.presence_of_all_elements_located)
-  driver.find_element_by_id('continueButton').click()
+  cur_url = driver.current_url
+  trim_url = str(cur_url).replace('https://','')
+  basic_auth_url = 'https://' + ID + '%40sic:' + PASSWORD + '@' + trim_url
+  driver.get(basic_auth_url)
   wait.until(EC.presence_of_all_elements_located)
+  next_button = driver.find_element_by_id('continueButton')
+  next_button.click()
+
 except Exception as e:
   error_flg = True
   print(e)
