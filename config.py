584e26a552b76f045fe6aff25690bed5216d73b8
yuto0618 <al18032@shibaura-it.ac.jp>
2020-06-22 14:00:10 +0900

    config

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ class Config:
 
   def open(self):
     config_layout = [
-      [sg.Text('設定')]
+      [sg.Text('設定')],[sg.Text('scombID')],[sg.Text('scombパスワード')],[sg.Button('終了')]
     ]
     config_window = sg.Window('設定',layout=config_layout,size=(200,200))
     while True:
       event , value = config_window.read()
-      if event is None:
+      if event in (None,'終了'):
+        print('ホーム画面に戻ります')
         break
     config_window.close()
