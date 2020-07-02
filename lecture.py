ff614b68bbbf0697ad555f10882186d91bcf1d36
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-07-03 00:18:38 +0900

    授業詳細画面から休講日を削除

--- lecture.py
+++ lecture.py
@@ -8,17 +8,16 @@ import PySimpleGUI as sg
 
 class Lecture:
 
-  def __init__(self,lecture_name,teacher_name,no_class_days):
+  def __init__(self,lecture_name,teacher_name):
     self.lecture_name = lecture_name
     self.teacher_name = teacher_name
-    self.no_class_days = no_class_days
 
   def open(self):
     layout = [
       [sg.Text(self.lecture_name)],
-      [sg.Text('zoomURL'),sg.InputText(key = 'zoom_url')],
       [sg.Text('担当講師'),sg.Text(self.teacher_name)],
-      [sg.Text('休講'),sg.Text(self.no_class_days)],
+      [sg.Text('zoomURL'),sg.InputText(key = 'zoom_url')],
+      #[sg.Text('休講'),sg.Text(self.no_class_days)],
       [sg.Button('OK')],
       [sg.Button('キャンセル')]
     ]
@@ -28,7 +27,7 @@ class Lecture:
       event ,value = window.read()
 
       if event == 'OK':
-        if len(values['zoom_url']) > 80:
+        if len(value['zoom_url']) > 80:
           show_message = 'zoomURLは80字以下にしてください'
           sg.popup(show_message)
 
